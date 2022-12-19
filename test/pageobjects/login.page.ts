076d43d4c778a8ec4d0af1e8be0228ccb12d4fc7
AkashSoni1992 <soniakash1992@gmail.com>
2022-12-19 18:55:13 +1100

    codebase

---
+++ test/pageobjects/login.page.ts
@@ -0,0 +1,47 @@
+import { ChainablePromiseElement } from 'webdriverio';
+
+
+/**
+ * sub page containing specific selectors and methods for a specific page
+ */
+class LoginPage {
+    /**
+     * define selectors using getter methods
+     */
+    // public get inputUsername () {
+    //     return $('#username');
+    // }
+
+    // public get inputPassword () {
+    //     return $('#password');
+    // }
+
+    // public get btnSubmit () {
+    //     return $('button[type="submit"]');
+    // }
+
+    /**
+     * a method to encapsule automation code to interact with the page
+     * e.g. to login using username and password
+     */
+    //  public async login (username: string, password: string) {
+    //     await this.inputUsername.setValue(username);
+    //     await this.inputPassword.setValue(password);
+    //     await this.btnSubmit.click();
+    // }
+
+    /**
+     * overwrite specific options to adapt it to page object
+     */
+    // public loginIcon: string = "~Login";
+    public loginIcon: string = '//android.widget.Button[@content-desc="Login"]/android.widget.TextView[2]';
+    public async open () {
+        //const LOGIN_ICON = '~Login';
+        await browser.pause(1000);
+        $(this.loginIcon).click();
+        await browser.pause(5000);
+        console.log("Inside application login page");
+    }
+}
+
+export default new LoginPage();
